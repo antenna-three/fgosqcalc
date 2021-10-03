2744a37ecbe64be04f41875164f819d2ef31f9b1
antenna-three <tkduc@outlook.com>
2021-10-03 11:13:32 +0900

    Add return value types for exported arrow function

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@chakra-ui/layout'
 import { VStack } from '@chakra-ui/react'
-import React, { FC } from 'react'
+import React, { ReactNode } from 'react'
 
-export const Card: FC = ({ children }) => (
+export const Card = ({ children }: { children: ReactNode }): JSX.Element => (
   <Box p={5} borderWidth="thin" borderRadius="md" shadow="md">
     <VStack alignItems="stretch" spacing={5}>
       {children}
--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,21 +1,24 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
-export const useLocalStorage = <T>(key: string, initialState: T): [T, Dispatch<SetStateAction<T>>] => {
-    const [state, setState] = useState(initialState)
-    useEffect(() => {
-        const json = localStorage.getItem(key)
-        if (json) {
-            const obj = JSON.parse(json)
-            if (typeof initialState == 'object') {
-                setState({...initialState, ...obj})
-            } else {
-                setState(obj)
-            }
-        }
-    }, [])
-    useEffect(() => {
-        const json = JSON.stringify(state)
-        localStorage.setItem(key, json)
-    }, [state])
-    return [state, setState]
+export const useLocalStorage = <T>(
+  key: string,
+  initialState: T
+): [T, Dispatch<SetStateAction<T>>] => {
+  const [state, setState] = useState(initialState)
+  useEffect(() => {
+    const json = localStorage.getItem(key)
+    if (json) {
+      const obj = JSON.parse(json)
+      if (typeof initialState == 'object') {
+        setState({ ...initialState, ...obj })
+      } else {
+        setState(obj)
+      }
+    }
+  }, [])
+  useEffect(() => {
+    const json = JSON.stringify(state)
+    localStorage.setItem(key, json)
+  }, [state])
+  return [state, setState]
 }
